b32a6fdb10be59182d38a57d83a6a1398eefcfb2
CarolineEicher <caroline.eicher@gmx.ch>
2024-05-15 18:52:24 +0200

    Start the code for getting the location IDs

--- Scripts/1_Setup.R
+++ Scripts/1_Setup.R
@@ -1,2 +1,19 @@
+#Load the necessary packages
 library(tidyverse)
 library(httr)
+
+# Load the API key
+api_key <- readr::read_csv("API_key.csv")$API_key
+
+# Get the location IDs
+url_search <- "https://api.content.tripadvisor.com/api/v1/location/search"
+
+queryString <- list(
+  searchQuery = "formentera"
+  category = "attractions",
+  language = "en"
+)
+
+response <- VERB("GET", url, query = queryString, content_type("application/octet-stream"), accept("application/json"))
+
+content(response, "text")
