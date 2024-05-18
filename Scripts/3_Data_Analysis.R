fd0d12d081a9c47e9af1187d7bab8fa1a7340cd7
CarolineEicher <caroline.eicher@gmx.ch>
2024-05-18 18:35:02 +0200

    Analyze by friends getaway and overall ratings

--- Scripts/3_Data_Analysis.R
+++ Scripts/3_Data_Analysis.R
@@ -6,6 +6,20 @@ library(readr)
 data_formentera <- read_csv("Data/data_formentera.csv")
 View(data_formentera)
 
+#What location was best rated?
+data_formentera |>
+  group_by(name) |>
+  summarise(mean_rating = mean(rating)) |>
+  arrange(desc(mean_rating)) |>
+  head(5)
+
+#What location was most rated?
+data_formentera |>
+  group_by(name) |>
+  summarise(n = n()) |>
+  arrange(desc(n)) |>
+  head(5)
+
 #What location were best rated by couples?
 data_formentera |>
   filter(trip_type == "Couples") |>
@@ -17,6 +31,22 @@ data_formentera |>
 #What location was most rated by couples?
 data_formentera |>
   filter(trip_type == "Couples") |>
+  group_by(name) |>
+  summarise(n = n()) |>
+  arrange(desc(n)) |>
+  head(5)
+
+#what location was best rated by friends?
+data_formentera |>
+  filter(trip_type == "Friends getaway") |>
+  group_by(name) |>
+  summarise(mean_rating = mean(rating)) |>
+  arrange(desc(mean_rating)) |>
+  head(5)
+
+#what location was most rated by friends?
+data_formentera |>
+  filter(trip_type == "Friends getaway") |>
   group_by(name) |>
   summarise(n = n()) |>
   arrange(desc(n)) |>
