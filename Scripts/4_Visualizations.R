9ba97c7ff413769ee48531a3dcbbda8ebaf1c5e7
CarolineEicher <caroline.eicher@gmx.ch>
2024-05-19 12:26:15 +0200

    Visualize the attractions best rated and most rated by couples.

---
+++ Scripts/4_Visualizations.R
@@ -0,0 +1,44 @@
+#Load packages
+library(tidyverse)
+library(ggplot2)
+
+#Load data
+data_formentera <- read_csv("Data/data_formentera.csv")
+View(data_formentera)
+
+#Plot top attractions rated by couples
+data_formentera |>
+  filter(trip_type == "Couples") |>
+  group_by(name) |>
+  summarise(mean_rating = mean(rating)) |>
+  arrange(desc(mean_rating)) |>
+  head(5) |> 
+  ggplot(aes(x = reorder(name, mean_rating), y = mean_rating)) +
+  geom_bar(stat = "identity", fill = "skyblue") +
+  coord_flip() +
+  labs(title = "Top 5 attractions in Formentera for couples",
+       x = "Attraction",
+       y = "Mean Rating") +
+  theme_minimal()
+
+#save the plot
+ggsave("Visualizations/attractions_best-rated-by-couples.png")
+
+
+#Plot attractions most rated by couples
+data_formentera |>
+  filter(trip_type == "Couples") |>
+  group_by(name) |>
+  summarise(n = n()) |>
+  arrange(desc(n)) |>
+  head(5) |> 
+  ggplot(aes(x = reorder(name, n), y = n)) +
+  geom_bar(stat = "identity", fill = "skyblue") +
+  coord_flip() +
+  labs(title = "Top 5 attractions in Formentera most rated by couples",
+       x = "Attraction",
+       y = "Number of Ratings") +
+  theme_minimal()
+
+#save the plot
+ggsave("Visualizations/attractions_most-rated-by-couples.png")
