5a578c5eabb5ea0cff4ec1f71afc9a08c59acfb4
CarolineEicher <caroline.eicher@gmx.ch>
2024-05-21 17:17:35 +0200

    create the UI

--- Traveller-App/app.R
+++ Traveller-App/app.R
@@ -5,7 +5,12 @@ library(bslib)
 ui <- page_sidebar(
   title = "Hello Traveller!",
   sidebar = sidebar("Choose your Travel Interests"),
-  "Travel Suggestions"
+  card(
+    card_header("Welcome to the Traveller App"),
+    "This app is designed to help you find the best travel destinations for your interests based on your 
+    fellow travellers experiences. Select your interests and we will show the best destinations for you!",
+    card_footer("Happy Travelling!")
+    )
 )
 
 # Define server logic ----
